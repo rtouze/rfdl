6c3593d16f910752ad8955d36900bd1f793fa1ea
Romain TOUZE <romain.touze@gmail.com>
2017-09-27 00:23:37 +0200

    Refactoring

--- rfdl.py
+++ rfdl.py
@@ -8,8 +8,12 @@ import requests
 import re
 from clint.textui import progress
 from argparse import ArgumentParser
+import sys
 
 CHUNK_S = 1024*1024
+
+session = requests.Session()
+
 
 def main():
     parser = ArgumentParser(description=__doc__)
@@ -20,25 +24,45 @@ def main():
 def download(url):
     """Download podcast as mp3 from url"""
     final_file = url.split("/")[-1] + ".mp3"
-    session = requests.Session()
+    mp3_url = get_mp3_url(url)
+    if not mp3_url:
+        print(f"No mp3 found in page {url}...")
+        sys.exit(1)
+    download_mp3_to_file(mp3_url, final_file)
 
+
+def get_mp3_url(url):
+    """Retrieve podcast mp3 url from broadcast page url """
     emission_content = session.get(url)
-    # on cherche data-asset-source="https://media.radiofrance-podcast.net/podcast09/10467-18.09.2017-ITEMA_21437558-1.mp3"
-    my_re = re.compile(r'data-asset-source="(https://media.radiofrance-podcast.net/.*\.mp3)"')
+    # We are looking for
+    # data-asset-source="https://media.radiofrance-podcast.net/some/path.mp3"
+    # for France Culture pr
+    # data_url="https://media.radiofrance-podcast.net/some/path.mp3" for France
+    # Inter
+    my_re = re.compile(
+        r'data-(?:asset-source|url)='
+        r'"(https://media.radiofrance-podcast.net/.*\.mp3)"'
+    )
     search_result = my_re.search(emission_content.text)
-    mp3_url = None
-    if search_result:
-        mp3_url = search_result.group(1)
+    return search_result.group(1) if search_result else None
 
-    with open(final_file, 'bw') as output:
+
+def download_mp3_to_file(mp3_url, final_file):
+    """Create the file from downloaded data,"""
+
+    with open(final_file, 'wb') as output:
         print(f"Downloading {mp3_url}")
         stream = session.get(mp3_url, stream=True)
-        size = int(stream.headers["Content-Length"]) if "Content-Length" in stream.headers else 0
+        size = (
+            int(stream.headers["Content-Length"]) 
+            if "Content-Length" in stream.headers else 0
+        )
 
         content_with_bar = progress.bar(
             stream.iter_content(chunk_size=CHUNK_S),
             expected_size=size/(CHUNK_S) + 1
         )
+
         for chunk in content_with_bar:
             output.write(chunk)
 
