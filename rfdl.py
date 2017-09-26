db4afb2bac83a4fdb75fa3b9d9f626e8fe38a4c1
Romain TOUZE <romain.touze@gmail.com>
2017-09-26 23:44:55 +0200

    Cleanup, add status bar and move file

--- rfdl.py
+++ rfdl.py
@@ -1,35 +1,37 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Downloader for radio france podcaast"""
+"""Downloader for radio france podcasts. Because they do not want to provide a
+link on podcast page..."""
 
 import requests
 import re
 import sys
+from clint.textui import progress
+
+CHUNK_S = 1024*1024
 
 
 def main():
-    print("go")
     url = sys.argv[1] if len(sys.argv) > 1 else "https://www.franceculture.fr/emissions/les-chemins-de-la-philosophie/philosophie-des-jeux-video-14-de-lantiquite-nos-jours"
     final_file = url.split("/")[-1] + ".mp3"
-    response = requests.get(url)
-    print(type(response.text))
+    session = requests.Session()
 
-
+    emission_content = session.get(url)
     # on cherche data-asset-source="https://media.radiofrance-podcast.net/podcast09/10467-18.09.2017-ITEMA_21437558-1.mp3"
     my_re = re.compile(r'data-asset-source="(https://media.radiofrance-podcast.net/.*\.mp3)"')
-    search_result = my_re.search(response.text)
+    search_result = my_re.search(emission_content.text)
     mp3_url = None
     if search_result:
-        print("j'ai un result")
         mp3_url = search_result.group(1)
 
     with open(final_file, 'bw') as output:
         print(f"Downloading {mp3_url}")
-        output.write(requests.get(mp3_url).content)
-        print(f"{output.name} downloaded!")
-    
-    pass
+        stream = session.get(mp3_url, stream=True)
+        size = int(stream.headers["Content-Length"]) if "Content-Length" in stream.headers else 0
+        for content in progress.bar(stream.iter_content(chunk_size=CHUNK_S), expected_size=size/(CHUNK_S)):
+            output.write(content)
+            output.flush()
 
 if __name__ == '__main__':
     main()
